01182ad44b06e1192b2583a78fa4324c0340b4ac
Antonio Chamal <155495153+tonyroxtar@users.noreply.github.com>
2024-02-23 00:40:20 +0000

    forgot the code :P

--- src/app.js
+++ src/app.js
@@ -7,5 +7,15 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our", "them"];
+  let adj = ["great", "big", "tiny"];
+  let noun = ["jogger", "racoon", "mutt"];
+  let domain = [".net", ".com", ".io", ".cl"];
+
+  for (let i = 0; i < pronoun.length; i++)
+    for (let j = 0; j < adj.length; j++)
+      for (let k = 0; k < noun.length; k++)
+        for (let l = 0; l < domain.length; l++) {
+          console.log(pronoun[i], adj[j], noun[k], domain[l]);
+        }
 };
